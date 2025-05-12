0c638f088d27f1a00aca3fd0cbb9413b4c6230ec
rakhimovcode <abrorbekr11@gmail.com>
2025-05-12 22:35:49 +0500

    surveyuz

--- src/admins/dto/create-admin.dto.ts
+++ src/admins/dto/create-admin.dto.ts
@@ -1,7 +1,16 @@
+import { IsEmail, IsNotEmpty, IsPhoneNumber, IsString } from "class-validator"
+
 export class CreateAdminDto {
+    @IsNotEmpty()
+    @IsString()
     full_name:string
+    @IsEmail()
+    @IsNotEmpty()
     email:string
+    @IsNotEmpty()
+    @IsPhoneNumber("UZ")
     phone_number:string
+    @IsNotEmpty()
+    @IsString()
     password:string
-    is_creator:boolean
 }
--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,35 @@
-import { Controller } from '@nestjs/common';
-import { AuthService } from './auth.service';
+import {
+  Controller,
+  Post,
+  Body,
+  Req,
+  Res,
+  HttpCode,
+  Get,
+} from "@nestjs/common";
+import { AuthService } from "./auth.service";
+import { SignInAdminDto } from "./dto/sign-in-admin.dto";
+import { Request, Response } from "express";
 
-@Controller('auth')
+@Controller("auth")
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post("sign-in")
+  @HttpCode(200)
+  async signIn(@Body() signInDto: SignInAdminDto, @Res({passthrough:true}) res: Response) {
+    return this.authService.signInAdmin(signInDto, res);
+  }
+
+  @Post("sign-out")
+  @HttpCode(200)
+  async signOut(@Req() req: Request, @Res({passthrough:true}) res: Response) {
+    return this.authService.signOutAdmin(req, res);
+  }
+
+  @Get("refresh")
+  @HttpCode(200)
+  async refreshToken(@Req() req: Request, @Res({passthrough:true}) res: Response) {
+    return this.authService.refresh_token_Admin(req, res);
+  }
 }
